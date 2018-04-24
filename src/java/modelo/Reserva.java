5d08a2b63614025854ec5b99f2b07ac3466da9a1
Rodrigo <rodrigo.perez@alumnos.uva.es>
2018-04-24 20:12:46 +0200

    Modelo de reservas

---
+++ src/java/modelo/Reserva.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author sergiosanzferrero
+ */
+public class Reserva {
+    private String id;
+    private String dni;
+    private String fechaInicio;
+    private String fechaFin;
+    
+    public Reserva(){
+        id="";
+        dni="";
+        fechaInicio="";
+        fechaFin="";               
+    }
+
+    public Reserva(String id, String dni, String fechaInicio, String fechaFin) {
+        this.id = id;
+        this.dni = dni;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    
+    
+    
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    
+    
+    
+}
